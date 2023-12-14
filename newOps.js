ad5da6fafe021c2538ecd0ec90e1022b71a06508
kadir-zeren <k4dzeren@gmail.com>
2023-12-14 08:55:01 +0300

    havada destr.

--- newOps.js
+++ newOps.js
@@ -52,3 +52,81 @@ const {
   car1: { nameCar1, modelCar1, engineCar1 },
   car2: { nameCar2, modelCar2, engineCar2 },
 } = cars;
+
+//* Example
+const team = [
+  {
+    name: "Josh",
+    surname: "Barry",
+    job: "developer",
+    age: 30,
+  },
+  {
+    name: "Josh",
+    surname: "Barry",
+    job: "tester",
+    age: 45,
+  },
+  {
+    name: "Hazel",
+    surname: "Nut",
+    job: "team lead",
+    age: 40,
+  },
+];
+//? KLASIK
+team.forEach((t) => {
+  console.log("NAME:", t.name);
+  console.log("SURNAME:", t.surname);
+  console.log("JOB:", t["job"]);
+  console.log("AGE:", t["age"]);
+  console.log("**************");
+});
+
+//? DESTR.
+team.forEach((person) => {
+  const { name, surname, job, age } = person;
+  console.log("NAME:", name);
+  console.log("SURNAME:", surname);
+  console.log("JOB:", job);
+  console.log("AGE:", age);
+  console.log("**************");
+});
+//!----  FUNCTIONLARDA DEATRUC. KULLANIMI ----
+
+//? Donus degeri olarak
+const getProduct = () => {
+  return {
+    id: "123",
+    pName: "NIKE",
+    price: 300,
+    stock: 1000,
+  };
+};
+
+// console.log(getProduct())
+let { pName, price, stock } = getProduct();
+//* function'un dondurdugu obje dogrudan dest. yapilabilir
+
+console.log("PRODUCT NAME:", pName, "PRICE:", price);
+
+stock = stock - 100;
+console.log("REMAINING STOCK:", stock);
+
+//? Parametre olarak
+const data = {
+  id: "123",
+  pName: "NIKE",
+  price: 300,
+};
+// const print = function (data) {
+//   const { id, pName, price } = data
+//   console.log(pName, price)
+// }
+
+const print = function ({ pName, price }) {
+  //? Yolda destr.
+  console.log(pName, price);
+};
+
+print(data);
